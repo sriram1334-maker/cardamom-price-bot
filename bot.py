e6523225b93d8fd2f8eb36a6d115f31850c37d0d
sriram1334-maker <sriram1334@gmail.com>
2026-08-27 03:38:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,15 @@ import os
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-message = """
-Hello from Cardamom Bot
+message ="""
+🟢 Cardamom Daily Price Update
+6 mm : ₹1800/kg
+7 mm : ₹2100/kg
+7+ mm : ₹2250/kg
+8 mm : ₹2500/kg
+8+ mm : ₹2900/kg
+Regards,
+CardoEla
 """
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
