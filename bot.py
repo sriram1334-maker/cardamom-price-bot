1da4907c54f1406f6eaadb1f49cd9fdfbd932e9c
sriram1334-maker <sriram1334@gmail.com>
2026-08-27 03:26:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,11 @@ import requests
 2
 import os
 3
- 
 4
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 5
 CHAT_ID = os.environ["CHAT_ID"]
 6
- 
 7
 message = """
 8
@@ -16,11 +14,9 @@ Hello from Cardamom Bot
 9
 """
 10
- 
 11
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 12
- 
 13
 requests.post(
 14
