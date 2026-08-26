7d90af66ada2f8a1c167b0552fd705f2533e1a0b
sriram1334-maker <sriram1334@gmail.com>
2026-08-27 03:55:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,23 +7,57 @@ CHAT_ID = os.environ["CHAT_ID"]
 
 URL = "https://www.indianspices.com/marketing/price/domestic/daily-price.html"
 
-tables = pd.read_html(URL)
+try:
+    tables = pd.read_html(URL)
 
-message = "📋 TABLE STRUCTURE\n\n"
+    table = tables[1]
 
-for i, table in enumerate(tables):
-    message += f"\nTable {i}\n"
-    message += str(table.head())
-    message += "\n\n"
+    row1 = table.iloc[2]
+    row2 = table.iloc[3]
+
+    message = f"""
+🌿 CardoEla Daily Auction Report
+
+📅 {row1[3]}
+
+🏢 {row1[1]}
+📦 Arrived Qty: {row1[4]} Kg
+✅ Sold Qty: {row1[5]} Kg
+💰 Avg Price: ₹{row1[8]}/Kg
+🚀 Max Price: ₹{row1[6]}/Kg
+
+━━━━━━━━━━━━━━
+
+🏢 {row2[1]}
+📦 Arrived Qty: {row2[4]} Kg
+✅ Sold Qty: {row2[5]} Kg
+💰 Avg Price: ₹{row2[8]}/Kg
+🚀 Max Price: ₹{row2[6]}/Kg
+
+📍 Source: Spices Board India
+"""
+
+except Exception as e:
+
+    message = f"""
+⚠️ CardoEla Alert
+
+Failed to fetch today's cardamom prices.
+
+Error:
+{str(e)}
+
+📍 Source: Spices Board India
+"""
 
 telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(
+response = requests.post(
     telegram_url,
     data={
         "chat_id": CHAT_ID,
-        "text": message[:4000]
+        "text": message
     }
 )
 
-print("Sent")
+print(response.text)
