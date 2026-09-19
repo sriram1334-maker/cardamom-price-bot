5eccf35307615f1c4d0c657aa11df422064d2648
sriram1334-maker <sriram1334@gmail.com>
2026-09-19 20:55:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.width', 1000)
+
 url = "https://www.indianspices.com/marketing/price/domestic/daily-price.html"
 
 table = pd.read_html(url)[1]
 
-print(table)
+print(table.to_string(index=False))
